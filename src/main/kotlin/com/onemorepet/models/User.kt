d2734e6c5b4f8013f2d9992619384d58b95db373
Михаил Лопатин <mix0z@MacBook-Air-Mihail.local>
2021-11-01 21:53:09 +0300

    Fix 11 and create 12

--- src/main/kotlin/com/onemorepet/models/User.kt
+++ src/main/kotlin/com/onemorepet/models/User.kt
@@ -3,5 +3,5 @@ package com.onemorepet.models
 data class User(
     val name: String,
     val location: Location,
-    val discounts: Discount? = null
+    val discounts: MutableList<Discount>? = null
 )
--- src/main/kotlin/com/onemorepet/services/DiscountService.kt
+++ src/main/kotlin/com/onemorepet/services/DiscountService.kt
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service
 @Service
 class DiscountService {
     fun calcPrice(user: User, offer: PetOffer): Int {
-        if (user.discounts == null) {
+        if (user.discounts == null || (!user.discounts.kind.equals(offer.kind) && user.discounts.kind != null)) {
             return offer.price
         }
         return (offer.price * user.discounts.value).toInt()
